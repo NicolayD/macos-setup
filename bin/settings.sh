87fb9ffe791d67cecbc6c727bc71c78afeacd933
Nikolay Dyulgerov <dyulgerov.nikolay@gmail.com>
2024-12-25 16:18:26 +0000

    Update settings.sh

--- bin/settings.sh
+++ bin/settings.sh
@@ -7,3 +7,9 @@ defaults write com.apple.dock "mru-spaces" -bool "false" && killall Dock
 # Safari
 # Restore tabs from last session
 defaults write com.apple.Safari AlwaysRestoreSessionAtLaunch -bool true
+
+# install Docker
+# curl -o Docker.dmg https://desktop.docker.com/mac/main/arm64/Docker.dmg
+# sudo hdiutil attach Docker.dmg
+# sudo /Volumes/Docker/Docker.app/Contents/MacOS/install
+# sudo hdiutil detach /Volumes/Docker
